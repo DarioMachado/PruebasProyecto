eb1d02e011bee7dc053457d437575e6e90533653
DarioMachado <115183065+DarioMachado@users.noreply.github.com>
2024-05-16 22:36:33 +0200

    2/3 en c#

---
+++ deberes csharp/dotnetBasico/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Del 1 al 20, ¿cuántos números de la secuencia de Fibonacci te gustaría saber?");
+
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+
+        if (cantidad <= 0 || cantidad > 20) {
+            Console.WriteLine("Ese número no está entre 1 y 20...");
+            return;
+        }
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
---
+++ deberes csharp/qrascii/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using QRCoder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Dime texto un texto o URL para convertir a QR");
+        string input = Console.ReadLine();
+
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.Q);
+        AsciiQRCode qrCode = new AsciiQRCode(qrCodeData);
+
+        Console.WriteLine(qrCode.GetGraphic(1));
+
+    }
+}
